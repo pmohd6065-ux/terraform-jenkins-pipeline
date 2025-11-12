86eab613fc898e2ce27245bcaf74af93444c7b7a
Mohd Parvez <pmohd6065@gmail.com>
2025-11-12 21:34:34 +0530

    Refactor aws_instance resource configuration

--- main.tf
+++ main.tf
@@ -1,17 +1,11 @@
 resource "aws_instance" "public_instance" {
  ami           = var.ami
  instance_type = var.instance_type
-
- tags = {
-   Name = var.name_tag,
- }
-
-}
-resource "aws_instance" "public_instance" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  subnet_id     = "subnet-03490ecccc463caff"  # 👈 replace with your actual subnet ID
+ subnet_id     = "subnet-03490ecccc463caff"  # 👈 replace with your actual subnet ID
   tags = {
     Name = var.name_tag
   }
 }
+ tags = {
+   Name = var.name_tag,
+ }
